ced616599989a419106bf60ad1d6d355ba234143
Zhuoran He <zhuo1angT@Outlook.com>
2021-02-08 12:16:46 +0800

    tests

--- exercises/tests/tests3.rs
+++ exercises/tests/tests3.rs
@@ -4,8 +4,6 @@
 // we expect to get when we call `is_even(5)`.
 // Execute `rustlings hint tests3` for hints :)
 
-// I AM NOT DONE
-
 pub fn is_even(num: i32) -> bool {
     num % 2 == 0
 }
@@ -16,11 +14,13 @@ mod tests {
 
     #[test]
     fn is_true_when_even() {
-        assert!();
+        let x = 42;
+        assert!(is_even(x));
     }
 
     #[test]
     fn is_false_when_odd() {
-        assert!();
+        let x = 42;
+        assert!(is_even(x));
     }
 }
